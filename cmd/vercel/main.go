05d63e40fbfe6149cd1526a243fc4497178b9c58
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-05-22 13:54:16 -0300

    refactor: logger into variable

--- cmd/vercel/main.go
+++ cmd/vercel/main.go
@@ -17,6 +17,8 @@ type VercelConfig struct {
 	OutputDirectory string `json:"outputDirectory"`
 }
 
+var logger = log.Default()
+
 func main() {
 	configPath := flag.String("c", "./vercel.json", "the path to the vercel.json file")
 	staticDir := flag.String("s", "./static", "the directory to copy static files from")
@@ -24,13 +26,13 @@ func main() {
 
 	configFile, err := os.ReadFile(*configPath)
 	if err != nil {
-		log.Fatalf("Unable to read vercel.json file due to:\n%s", err)
+		logger.Fatalf("Unable to read vercel.json file due to:\n%s", err)
 	}
 
 	var c VercelConfig
 	err = json.Unmarshal(configFile, &c)
 	if err != nil {
-		log.Fatalf("Unable to parse vercel.json file due to:\n%s", err)
+		logger.Fatalf("Unable to parse vercel.json file due to:\n%s", err)
 	}
 
 	w := internals.StaticWriter{
@@ -41,21 +43,21 @@ func main() {
 		Logger:    *log.Default(),
 	}
 
-	log.Print("Writing static files")
+	logger.Print("Writing static files")
 	err = w.WriteAll()
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 
-	log.Print("Starting server")
+	logger.Print("Starting server")
 	mux := http.NewServeMux()
 
 	config.APIROUTES(mux)
 	mux.Handle("/", http.FileServer(http.Dir(c.OutputDirectory)))
 
-	log.Printf("Running server at port: %v", *port)
+	logger.Printf("Running server at port: %v", *port)
 	err = http.ListenAndServe(fmt.Sprintf(":%v", *port), mux)
 	if err != nil {
-		log.Fatalf("Server crashed due to:\n%s", err)
+		logger.Fatalf("Server crashed due to:\n%s", err)
 	}
 }
